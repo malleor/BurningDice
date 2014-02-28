ee0881ddd03f2501782dc606981aa5990c35e89e
slovic <slovic.r@gmail.com>
2014-02-28 23:19:54 +0100

    init

---
+++ BurnDicePlus/src/main/java/com/agapep/burndiceplus/MainActivity.java
@@ -0,0 +1,145 @@
+package com.agapep.burndiceplus;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.widget.TextView;
+
+import us.dicepl.android.sdk.BluetoothManipulator;
+import us.dicepl.android.sdk.DiceConnectionListener;
+import us.dicepl.android.sdk.DiceController;
+import us.dicepl.android.sdk.DiceResponseAdapter;
+import us.dicepl.android.sdk.DiceResponseListener;
+import us.dicepl.android.sdk.DiceScanningListener;
+import us.dicepl.android.sdk.Die;
+import us.dicepl.android.sdk.responsedata.RollData;
+
+public class MainActivity extends Activity {
+    private static final int[] developerKey = new int[] {0x83, 0xed, 0x60, 0x0e, 0x5d, 0x31, 0x8f, 0xe7};
+    private static final String TAG = "DICEPlus";
+    private Die dicePlus;
+    TextView TVResult;
+
+    DiceScanningListener scanningListener = new DiceScanningListener() {
+        @Override
+        public void onNewDie(Die die) {
+            Log.d(TAG, "New DICE+ found");
+            dicePlus = die;
+            DiceController.connect(dicePlus);
+        }
+
+        @Override
+        public void onScanStarted() {
+            Log.d(TAG, "Scan Started");
+        }
+
+        @Override
+        public void onScanFailed() {
+            Log.d(TAG, "Scan Failed");
+            BluetoothManipulator.startScan();
+        }
+
+        @Override
+        public void onScanFinished() {
+            Log.d(TAG, "Scan Finished");
+
+            if(dicePlus == null) {
+                BluetoothManipulator.startScan();
+            }
+        }
+    };
+
+    DiceConnectionListener connectionListener = new DiceConnectionListener() {
+        @Override
+        public void onConnectionEstablished(Die die) {
+            Log.d(TAG, "DICE+ Connected");
+
+            // Signing up for roll events
+            DiceController.subscribeRolls(dicePlus);
+        }
+
+        @Override
+        public void onConnectionFailed(Die die, Exception e) {
+            Log.d(TAG, "Connection failed", e);
+
+            dicePlus = null;
+
+            BluetoothManipulator.startScan();
+        }
+
+        @Override
+        public void onConnectionLost(Die die) {
+            Log.d(TAG, "Connection lost");
+
+            dicePlus = null;
+
+            BluetoothManipulator.startScan();
+        }
+    };
+
+    DiceResponseListener responseListener = new DiceResponseAdapter() {
+        @Override
+        public void onRoll(Die die, RollData rollData, Exception e) {
+            super.onRoll(die, rollData, e);
+
+            Log.d(TAG, "Roll: " + rollData.face);
+
+            final int face = rollData.face;
+
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    TVResult.setText(""+face);
+                }
+            });
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(TAG, "onResume");
+
+        TVResult = (TextView) findViewById(R.id.TVResult);
+
+        // Initiating
+        BluetoothManipulator.initiate(this);
+        DiceController.initiate(developerKey);
+
+        // Listen to all the state occurring during the discovering process of DICE+
+        BluetoothManipulator.registerDiceScanningListener(scanningListener);
+
+        // When connecting to DICE+ you get two responses: a good one and a bad one ;)
+        DiceController.registerDiceConnectionListener(connectionListener);
+
+        // Attaching to DICE+ events that we subscribed to.
+        DiceController.registerDiceResponseListener(responseListener);
+
+        // Scan for a DICE+
+        BluetoothManipulator.startScan();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(TAG, "onStop");
+
+        // Unregister all the listeners
+        DiceController.unregisterDiceConnectionListener(connectionListener);
+        BluetoothManipulator.unregisterDiceScanningListener(scanningListener);
+        DiceController.unregisterDiceResponseListener(responseListener);
+
+        DiceController.disconnectDie(dicePlus);
+        dicePlus = null;
+    }
+
+}
